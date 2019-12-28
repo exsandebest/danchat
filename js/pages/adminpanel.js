e208eae9d4632c1da3f1353298902deaedea615d
Daniil Bogdanov <51285775+exsandebest@users.noreply.github.com>
2019-12-29 01:37:25 +0300

    To SQL tmp

--- js/pages/adminpanel.js
+++ js/pages/adminpanel.js
@@ -1,42 +1,8 @@
-var gxhr = new XMLHttpRequest();
-gxhr.open("GET", "/admin/toMakeAdmin/list", true);
-gxhr.onload = () => {
-   var users = JSON.parse(gxhr.responseText);
-   users.forEach((user) => {
-      var option = document.createElement("option");
-      option.value = user;
-      option.innerHTML = user;
-      document.getElementById("userselector").appendChild(option);
-   })
-}
-gxhr.send();
-
-
-
-
-var gxhr2 = new XMLHttpRequest();
-gxhr2.open("GET", "/admin/toMakeUser/list", true);
-gxhr2.onload = () => {
-   var admins = JSON.parse(gxhr2.responseText);
-   admins.forEach((admin) => {
-      if (admin === "") {
-         admins.splice(admins.indexOf(admin), 1);
-      } else {
-         var option = document.createElement("option");
-         option.value = admin;
-         option.innerHTML = admin;
-         document.getElementById("adminselector").appendChild(option);
-      }
-   })
-}
-gxhr2.send();
-
-
 function sendMessage() {
    var message = document.getElementById("message").value;
    var xhr = new XMLHttpRequest();
    xhr.open("POST", "/admin/message", true);
-   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.setRequestHeader("Content-Type", "application/json");
    xhr.onload = () => {
       alert(xhr.responseText);
    }
@@ -46,28 +12,26 @@ function sendMessage() {
 
 }
 
-
-
 function makeAdmin() {
-   var user = document.getElementById("userselector").value;
+   var user = document.getElementById("inputLogin").value;
    var xhr = new XMLHttpRequest();
    xhr.open("POST", "/admin/make/admin", true);
-   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.setRequestHeader("Content-Type", "application/json");
    xhr.onload = () => {
       alert(xhr.responseText);
-      location.reload();
    }
-   xhr.send("user=" + user);
+   xhr.send(JSON.stringify({user : user}));
 }
 
 function makeUser() {
    var login = document.getElementById("adminselector").value;
    var xhr = new XMLHttpRequest();
    xhr.open("POST", "/admin/make/user", true);
-   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.setRequestHeader("Content-Type", "application/json");
    xhr.onload = () => {
       alert(xhr.responseText);
-      location.reload();
    }
-   xhr.send("login=" + login);
+   xhr.send(JSON.stringify({
+      login : login
+   }));
 }
