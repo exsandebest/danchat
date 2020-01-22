b44fa1f9fe94584c3753c7fba061c993b89d3233
Daniil Bogdanov <51285775+exsandebest@users.noreply.github.com>
2020-01-22 15:05:03 +0300

    Fix symbol counter

--- public/js/pages/chat.js
+++ public/js/pages/chat.js
@@ -57,6 +57,7 @@ function sendMessage() {
                errorChatPage(data.text || data, "sm");
             } else {
                elem.value = "";
+               document.getElementById("symbolsCounter").innerText = "1000/1000";
             }
          }).catch(err => errorChatPage(err, "sm"));
       }
